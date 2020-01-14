c94ef141b3fe44adac33a1dc3f73725bae8bc6ef
alifarina17 <farina@infoobjects.com>
2020-01-14 16:02:40 +0100

    Adding button

--- SkySearch/app/src/main/java/com/book/skysearch/LoginSignupScreen.java
+++ SkySearch/app/src/main/java/com/book/skysearch/LoginSignupScreen.java
@@ -1,5 +1,6 @@
 package com.book.skysearch;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,14 +22,6 @@ public class LoginSignupScreen extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -51,5 +44,10 @@ public class LoginSignupScreen extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login(View view) {
+        Intent goForward = new Intent(LoginSignupScreen.this, FlightSearch.class);
+        startActivity(goForward);
     }
 }
